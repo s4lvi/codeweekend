fa2468a1dcef948cbef109986499ace100cdf0c5
s4lvi <salvicomputers@gmail.com>
2021-11-21 14:08:32 -0500

    Created game functions

--- forg/src/pages/game.js
+++ forg/src/pages/game.js
@@ -9,39 +9,47 @@ class Game extends React.Component {
         height: "100%",
         type: Phaser.AUTO,
         scene: {
-          init: function() {
-            this.cameras.main.setBackgroundColor('#fff');
-            this.load.setCORS('anonymous');
-          },
-          preload: function(){
-            this.load.image('forg', 'https://64.media.tumblr.com/tumblr_l6ncfsPLYQ1qzj5ggo1_1280.jpg');
-          },
-          create: function() {
-            this.helloWorld = this.add.text(
-              this.cameras.main.centerX, 
-              this.cameras.main.centerY, 
-              "OOOOOOH FUC K ITS CODE WEEKEEND!!!!!!", { 
-                font: "40px Arial", 
-                fill: "#000" 
-              }
-            );
-            this.helloWorld.setOrigin(0.5);
-            
-            this.forg = this.add.sprite(
-              this.cameras.main.centerX, 
-              this.cameras.main.centerY, 
-              'forg'
-            );
-            this.forg.setScale(0.1);
-            this.forg.setOrigin(0.5);
-
-          },
-          update: function() {
-            this.helloWorld.angle += 1;
-            this.forg.angle -= 1;
-          }
+          init: this.init,
+          preload: this.preload,
+          create: this.create,
+          update: this.update
         }
       }
+
+    init() {
+      this.cameras.main.setBackgroundColor('#fff');
+      this.load.setCORS('anonymous');
+    }
+
+    preload() {
+      this.load.image('forg', 'https://64.media.tumblr.com/tumblr_l6ncfsPLYQ1qzj5ggo1_1280.jpg');
+    }
+
+    create() {
+      this.helloWorld = this.add.text(
+        this.cameras.main.centerX, 
+        this.cameras.main.centerY, 
+        "OOOOOOH FUC K ITS CODE WEEKEEND!!!!!!", { 
+          font: "40px Arial", 
+          fill: "#000" 
+        }
+      );
+      this.helloWorld.setOrigin(0.5);
+      
+      this.forg = this.add.sprite(
+        this.cameras.main.centerX, 
+        this.cameras.main.centerY, 
+        'forg'
+      );
+      this.forg.setScale(0.1);
+      this.forg.setOrigin(0.5);
+
+    }
+
+    update() {
+      this.helloWorld.angle += 1;
+      this.forg.angle -= 1;
+    }
 
     constructor(props) {
         super(props);
