3828e4937166bf171e756053d459f5f8867c60ce
s4lvi <salvicomputers@gmail.com>
2021-11-20 23:39:28 -0500

    added game component for real

---
+++ forg/src/pages/game.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                FUCK!!!!
+            </div>
+        )
+    }
+}
+
+export default Game
