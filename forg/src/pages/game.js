d2e27f7bc697176acd0fa59446897084615c2e56
s4lvi <salvicomputers@gmail.com>
2021-11-21 01:07:47 -0500

    added ion-phaser

--- forg/src/pages/game.js
+++ forg/src/pages/game.js
@@ -1,16 +1,41 @@
 import * as React from "react"
-import * as PIXI from "pixi.js"
+import Phaser from 'phaser'
+import { IonPhaser } from '@ion-phaser/react'
 
 class Game extends React.Component {
+
+    game = {
+        width: "100%",
+        height: "100%",
+        type: Phaser.AUTO,
+        scene: {
+          init: function() {
+            this.cameras.main.setBackgroundColor('#24252A')
+          },
+          create: function() {
+            this.helloWorld = this.add.text(
+              this.cameras.main.centerX, 
+              this.cameras.main.centerY, 
+              "OOOOOOH FUC K ITS CODE WEEKEEND!!!!!!", { 
+                font: "40px Arial", 
+                fill: "#ffffff" 
+              }
+            );
+            this.helloWorld.setOrigin(0.5);
+          },
+          update: function() {
+            this.helloWorld.angle += 1;
+          }
+        }
+      }
+
     constructor(props) {
         super(props);
     }
 
     render() {
         return(
-            <div>
-                FUCK!!!!
-            </div>
+            <IonPhaser game={this.game} />
         )
     }
 }
